c2f0c978dd8f67484557ef5ac61691c310819db0
Abhishek-Dhamija <abhishekdhamija2@gmail.com>
2025-01-19 19:28:05 +0530

    Update model_helper.py

--- Download files/model_helper.py
+++ Download files/model_helper.py
@@ -3,16 +3,16 @@ from torch import nn
 from torchvision import models, transforms
 from PIL import Image
 
-
+# Define the class names
 class_names = ['Front Breakage', 'Front Crushed', 'Front Normal', 'Rear Breakage', 'Rear Crushed', 'Rear Normal']
 trained_model = None
-
 
 # Load the pre-trained ResNet model
 class CarClassifierResNet(nn.Module):
     def __init__(self, num_classes=6):
         super().__init__()
         self.model = models.resnet50(weights='DEFAULT')
+
         # Freeze all layers except the final fully connected layer
         for param in self.model.parameters():
             param.requires_grad = False
@@ -21,7 +21,7 @@ class CarClassifierResNet(nn.Module):
         for param in self.model.layer4.parameters():
             param.requires_grad = True
 
-            # Replace the final fully connected layer
+        # Replace the final fully connected layer
         self.model.fc = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(self.model.fc.in_features, num_classes)
@@ -33,24 +33,31 @@ class CarClassifierResNet(nn.Module):
 
 
 def predict(image_path):
+    # Ensure the image is in RGB format
     image = Image.open(image_path).convert("RGB")
+
     # Image transformation for input preprocessing
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-
     image_tensor = transform(image).unsqueeze(0)
 
-    global trained_model
+    # Select the device (CPU in this case)
+    device = torch.device('cpu')
 
+    global trained_model
     if not trained_model:
         trained_model = CarClassifierResNet()
-        trained_model.load_state_dict(torch.load("model/saved_model.pth"))
-        trained_model.eval()
+        # Load the saved model weights onto the CPU
+        trained_model.load_state_dict(torch.load("model/saved_model.pth", map_location=device))
+        trained_model.eval()  # Set model to evaluation mode
+        trained_model.to(device)  # Move model to the appropriate device
 
+    # Perform inference
     with torch.no_grad():
+        image_tensor = image_tensor.to(device)  # Ensure the image tensor is on the same device
         output = trained_model(image_tensor)
         _, predicted_class = torch.max(output, 1)
         return class_names[predicted_class.item()]
